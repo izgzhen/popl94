cf57033904046f271a71aa5d5f971db43ae00838
Zhen Zhang <izgzhen@gmail.com>
2016-01-31 01:01:54 +0800

    define undefined

--- src/Translate.hs
+++ src/Translate.hs
@@ -1,6 +1,6 @@
 -- Translate from the source language to target lambda calculus
 {-# LANGUAGE RecordWildCards, TypeSynonymInstances,
-             FlexibleInstances #-}
+             FlexibleInstances, TemplateHaskell #-}
 
 module Translate where
 
@@ -15,8 +15,16 @@ import Control.Monad.Except
 import qualified Data.Map as M
 import qualified Data.Set as S
 import Data.Set (union, intersection)
+import Control.Lens
 
-type TEState = M.Map Name (Either SimpleTS CompoundTS, Place)
+data TEState = TEState {
+  _tyDict       :: M.Map Name (Either SimpleTS CompoundTS, Place)
+, _tyCounter    :: Int
+, _pCounter     :: Int
+, _effCounter   :: Int
+}
+
+makeLenses ''TEState
 
 type TE = ExceptT String (State TEState)
 
@@ -80,7 +88,7 @@ translate (S.ELetrec f x expr1 expr2) = do
     p <- newPlaceVar
     (T.EAbs x expr1' p', (ty', _), eff1, subst1)
         <- withCompoundType f (CTy ty, p) $ translate (S.EAbs x expr1)
-    let ps = frv (ty `substIn` subst1 :: Type)
+    let ps = S.toList $ frv (ty `substIn` subst1 :: Type)
     refineEnv subst1
     ty'' <- tyClosure subst1 ty'
     (expr2', decTy, eff2, subst2)
@@ -98,67 +106,150 @@ translate' expr = do
 -- -- Utils
 
 lookupSimple :: Name -> TE (SimpleTS, Place)
-lookupSimple = undefined
+lookupSimple x = do
+    dict <- _tyDict <$> get
+    case M.lookup x dict of
+        Just (Left sts, p) -> return (sts, p)
+        _ -> throwError $ "can't find SimpleTS " ++ x
 
 lookupCompound :: Name -> TE (CompoundTS, Place)
-lookupCompound = undefined
+lookupCompound x = do
+    dict <- _tyDict <$> get
+    case M.lookup x dict of
+        Just (Right cts, p) -> return (cts, p)
+        _ -> throwError $ "can't find CompoundTS " ++ x
 
 newTyVar :: TE Type
-newTyVar = undefined
+newTyVar = do
+    i <- use tyCounter
+    tyCounter .= i + 1
+    return $ TVar ("_ty" ++ show i)
 
 newEffVar :: TE EVar
-newEffVar = undefined
+newEffVar = do
+    i <- use effCounter
+    effCounter .= i + 1
+    return $ EVar ("_eff" ++ show i)
 
 newPlaceVar :: TE Place
-newPlaceVar = undefined
+newPlaceVar = do
+    i <- use pCounter
+    pCounter .= i + 1
+    return $ PVar ("_p" ++ show i)
 
 withCompoundType :: Name -> (CompoundTS, Place) -> TE a -> TE a
-withCompoundType = undefined
+withCompoundType x (cts, p) action = do
+    oldDict <- use tyDict
+    tyDict %= M.insert x (Right cts, p)
+    ret <- action
+    tyDict .= oldDict
+    return ret
 
 withSimpleType :: Name -> (SimpleTS, Place) -> TE a -> TE a
-withSimpleType = undefined
+withSimpleType x (sts, p) action = do
+    oldDict <- use tyDict
+    tyDict %= M.insert x (Left sts, p)
+    ret <- action
+    tyDict .= oldDict
+    return ret
 
-instSimple :: SimpleTS -> m (Type, Substitution)
-instSimple = undefined
+instSimple :: SimpleTS -> TE (Type, Substitution)
+instSimple sts = do
+    let c@CanonType{..} = toCanonType sts
+    ts <- sequence (replicate (length _tvars) newTyVar)
+    es <- sequence (replicate (length _evars) newEffVar)
+    let subst = emptySubst {
+      _typeSubst   = M.fromList $ zip _tvars ts
+    , _effectSubst = M.fromList $ zip (map EVar _evars) (map (\e -> (e, emptyEffects)) es)
+    }
+    return (_innerty, subst)
 
-instCompound :: CompoundTS -> m (Type, Substitution)
-instCompound = undefined
+instCompound :: CompoundTS -> TE (Type, Substitution)
+instCompound cts = do
+    let c@CanonType{..} = toCanonType cts
+    ts <- sequence (replicate (length _tvars) newTyVar)
+    es <- sequence (replicate (length _evars) newEffVar)
+    ps <- sequence (replicate (length _pvars) newPlaceVar)
+    let subst = emptySubst {
+      _typeSubst   = M.fromList $ zip _tvars ts
+    , _effectSubst = M.fromList $ zip (map EVar _evars) (map (\e -> (e, emptyEffects)) es)
+    , _placeSubst  = M.fromList $ zip _pvars ps
+    }
+    return (_innerty, subst)
 
 observe :: Fv a => a -> Effects -> TE Effects
 observe a effects = return $
-    S.fromList [ AEPut (PVar p) | p <- frv a ] `union`
-    S.fromList [ AEGet (PVar p) | p <- frv a ] `union`
-    S.fromList [ AEVar x | x <- fev a ] `intersection`
+    S.fromList [ AEPut (PVar p) | p <- S.toList $ frv a ] `union`
+    S.fromList [ AEGet (PVar p) | p <- S.toList $ frv a ] `union`
+    S.fromList [ AEVar x | x <- S.toList $ fev a ] `intersection`
     effects
 
 class Fv a where
     -- Free region variables
-    frv :: a -> [Name]
+    frv :: a -> S.Set Name
 
     -- Free effect variables
-    fev :: a -> [Name]
+    fev :: a -> S.Set Name
 
-instance Fv Effects where
-    frv = undefined
-    fev = undefined
+instance Fv a => Fv (S.Set a) where
+    frv = S.unions . map frv . S.toList
+    fev = S.unions . map fev . S.toList
 
-instance Fv DecoratedType where
-    frv = undefined
-    fev = undefined
+instance (Fv a, Fv b) => Fv (a, b) where
+    frv (a, b) = frv a `S.union` frv b
+    fev (a, b) = fev a `S.union` fev b
+
+instance Fv Effect where
+    frv (AEGet (PVar x)) = S.singleton x
+    frv (AEPut (PVar x)) = S.singleton x
+    frv _ = S.empty
+
+    fev (AEVar x) = S.singleton x
+    fev _ = S.empty
+
+instance Fv EVar where
+    frv _ = S.empty
+    fev (EVar x) = S.singleton x
+
+instance Fv Place where
+    frv (PVar x) = S.singleton x
+    frv _ = S.empty
+
+    fev _ = S.empty
 
 instance Fv Type where
-    frv = undefined
-    fev = undefined
+    frv (TArrow decTy1 arrEff decTy2) =
+        frv decTy1 `S.union`
+        frv arrEff `S.union`
+        frv decTy2
+    frv _ = S.empty
+
+    fev (TArrow decTy1 arrEff decTy2) =
+        fev decTy1 `S.union`
+        fev arrEff `S.union`
+        fev decTy2
+    fev _ = S.empty
 
 
 refineEnv :: Substitution -> TE ()
-refineEnv = undefined
+refineEnv s = tyDict %= flip (foldr f) (M.toList $ _typeSubst s)
+    where
+        f (x, ty) dict =
+            case unsafeLookup x dict of
+                (Right _, p) -> M.insert x (Right (CTy ty), p) dict
+                (Left  _, p) -> M.insert x (Left  (STy ty), p) dict
 
 tyClosure :: Substitution -> Type -> TE Type
-tyClosure = undefined
+tyClosure subst ty = undefined
 
 
 fromJust :: MonadError String m => Maybe a -> m a
 fromJust (Just a) = return a
 fromJust Nothing  = throwError "fromJust Nothing"
+
+
+unsafeLookup :: (Ord k, Show k) => k -> M.Map k v -> v
+unsafeLookup k m = case M.lookup k m of
+    Just v -> v
+    Nothing -> error $ "can't find " ++ show k
 
