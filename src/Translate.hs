9496953dc6f2e4d2409d4a73313a948afac45454
Zhen Zhang <izgzhen@gmail.com>
2016-01-30 21:12:04 +0800

    Unify

--- src/Translate.hs
+++ src/Translate.hs
@@ -1,6 +1,6 @@
 -- Translate from the source language to target lambda calculus
-{-# LANGUAGE RecordWildCards, MultiParamTypeClasses,
-             TypeSynonymInstances, FlexibleInstances #-}
+{-# LANGUAGE RecordWildCards, TypeSynonymInstances,
+             FlexibleInstances #-}
 
 module Translate where
 
@@ -8,6 +8,7 @@ import qualified Target as T
 import qualified Source as S
 import Common
 import Type
+import Unify
 
 import Control.Monad.State
 import Control.Monad.Except
@@ -15,35 +16,9 @@ import qualified Data.Map as M
 import qualified Data.Set as S
 import Data.Set (union, intersection)
 
-data TEState = TEState {
-  _typeOf   :: M.Map Name (Either SimpleTS CompoundTS, Place)
-}
+type TEState = M.Map Name (Either SimpleTS CompoundTS, Place)
 
 type TE = ExceptT String (State TEState)
-
-data Substitution = Substitution {
-  _placeSubst  :: M.Map Name Place
-, _typeSubst   :: M.Map Name CanonType
-, _effectSubst :: M.Map Name (Name, Effects)
-}
-
-unionSubst :: Substitution -> Substitution -> Substitution
-unionSubst = undefined
-
-emptySubst :: Substitution
-emptySubst = undefined
-
-class Subst a b where
-    substIn :: MonadError String m => a -> Substitution -> m b
-
-instance Subst Type CanonType where
-    substIn = undefined
-
-instance Subst Place Place where
-    substIn = undefined
-
-instance Subst Name (Name, Effects) where
-    substIn = undefined
 
 -- TE ⊢ e ⇒ e′ : μ, ϕ
 -- in TE, e translates to e′, which has type and place μ and effect ϕ
@@ -56,79 +31,79 @@ translate (S.EVar x) = do
     ts <- sequence $ replicate (length _tvars) newTyVar
     es <- sequence $ replicate (length _evars) newEffVar
     let subst = Substitution M.empty
-                             (M.fromList $ zip _tvars $ map (\t -> CanonType [] [] [] t) ts)
+                             (M.fromList $ zip _tvars $ ts)
                              (M.fromList $ zip _evars (map (\e -> (e, emptyEffects)) es))
     return (T.EVar x, (_innerty, p), emptyEffects, subst)
 
+-- Rule 19
+-- translate (S.EFun f) = do
+--     (cts, p') <- lookupCompound f
+--     let c = toCanonType cts
+--     (ty, subst) <- getSubst cts
+--     p <- newPlaceVar
+--     let eff = S.fromList [AEGet p', AEPut p]
+--     ps <- mapM (flip substIn subst) (map PVar $ _pvars c)
+--     return (T.EFun f ps p, (ty, p), eff, subst)
+
 -- Rule 15
-translate (S.EAbs x expr) = do
-    ty1 <- newTyVar     -- Concrete type, no polymorphism
-    p1 <- newPlaceVar
-    (expr', decTy2, eff, subst) <- withCompoundType x (CTy ty1, p1) $ translate expr
-    CanonType [] [] [] ty1' <- ty1 `substIn` subst
-    p1' <- p1 `substIn` subst
-    let decTy1 = (ty1', p1')
-    evar <- newEffVar
-    p <- newPlaceVar
-    return (T.EAbs x expr' p, (TArrow decTy1 (evar, eff) decTy2, p), S.singleton (AEPut p), subst)
+-- translate (S.EAbs x expr) = do
+--     ty1 <- newTyVar     -- Concrete type, no polymorphism
+--     p1 <- newPlaceVar
+--     (expr', decTy2, eff, subst) <- withCompoundType x (CTy ty1, p1) $ translate expr
+--     let ty1' = ty1 `substIn` subst
+--     let p1'  = p1 `substIn` subst
+--     let decTy1 = (ty1', p1')
+--     evar <- newEffVar
+--     p <- newPlaceVar
+--     return (T.EAbs x expr' p, (TArrow decTy1 (evar, eff) decTy2, p), S.singleton (AEPut p), subst)
 
 -- Rule 16
-translate e@(S.EApp expr1 expr2) = do
-    (expr1', (ty1, p), eff1, subst1) <- translate expr1
-    (expr2', decTy2,   eff2, subst2) <- translate expr2
-    case ty1 of
-        TArrow decTy2' (evar, eff0) decTy
-            | decTy2 == decTy2' -> do
-                let eff = eff0 `union` eff1 `union` eff2 `union` S.singleton (AEGet p)
-                return (T.EApp expr1' expr2', decTy, eff, subst1 `unionSubst` subst2)
-        _ -> throwError $ "can't type " ++ show e
+-- translate e@(S.EApp expr1 expr2) = do
+--     (expr1', (ty1, p), eff1, subst1) <- translate expr1
+--     (expr2', decTy2,   eff2, subst2) <- translate expr2
+--     case ty1 of
+--         TArrow decTy2' (evar, eff0) decTy
+--             | decTy2 == decTy2' -> do
+--                 let eff = eff0 `union` eff1 `union` eff2 `union` S.singleton (AEGet p)
+--                 return (T.EApp expr1' expr2', decTy, eff, subst1 `unionSubst` subst2)
+--         _ -> throwError $ "can't type " ++ show e
 
 -- Rule 17
-translate (S.ELet x expr1 expr2) = do
-    (expr1', (ty1, p1), eff1, subst1) <- translate expr1
-    tyx <- newTyVar
-    (expr2', decTy, eff2, subst2) <- withSimpleType x (STy tyx, p1) $ translate expr2
-    return (T.ELet x expr1' expr2', decTy, eff1 `S.union` eff2, subst1 `unionSubst` subst2)
+-- translate (S.ELet x expr1 expr2) = do
+--     (expr1', (ty1, p1), eff1, subst1) <- translate expr1
+--     tyx <- newTyVar
+--     (expr2', decTy, eff2, subst2) <- withSimpleType x (STy tyx, p1) $ translate expr2
+--     return (T.ELet x expr1' expr2', decTy, eff1 `S.union` eff2, subst1 `unionSubst` subst2)
 
 -- Rule 18: f can be used region-polymorphically, but not
 --          type-polymorphically
-translate (S.ELetrec f x expr1 expr2) = do
-    ty <- newTyVar
-    PVar px <- newPlaceVar
-    PVar pret <- newPlaceVar
-    let cty = CForallPlc px (CForallPlc pret (CTy ty))
-    (T.EAbs x expr1' px', (_, px), eff1, subst1)
-        <- withCompoundType f (cty, PVar pret)
-            $ translate (S.EAbs x expr1)
-    c1 <- ty `substIn` subst1
-    when (px /= px') $ throwError "px' /= px"
-    ty' <- newTyVar
-    (expr2', decTy, eff2, subst2)
-        <- withCompoundType f (CTy ty', px) $ translate expr2
-    return $ ( T.ELetrec f (_pvars c1) x px expr1' expr2'
-             , decTy
-             , eff1 `union` eff2
-             , subst1 `unionSubst` subst2 )
+-- translate (S.ELetrec f x expr1 expr2) = do
+--     ty <- newTyVar
+--     PVar px <- newPlaceVar
+--     PVar pret <- newPlaceVar
+--     let cty = CForallPlc px (CForallPlc pret (CTy ty))
+--     (T.EAbs x expr1' px', (_, px), eff1, subst1)
+--         <- withCompoundType f (cty, PVar pret)
+--             $ translate (S.EAbs x expr1)
+--     let c1 = ty `substIn` subst1
+--     when (px /= px') $ throwError "px' /= px"
+--     ty' <- newTyVar
+--     (expr2', decTy, eff2, subst2)
+--         <- withCompoundType f (CTy ty', px) $ translate expr2
+--     return $ ( T.ELetrec f (_pvars c1) x px expr1' expr2'
+--              , decTy
+--              , eff1 `union` eff2
+--              , subst1 `unionSubst` subst2 )
 
--- Rule 19
-translate (S.EFun f) = do
-    (cts, p') <- lookupCompound f
-    let c = toCanonType cts
-    (ty, subst) <- getSubst cts
-    p <- newPlaceVar
-    let eff = S.fromList [AEGet p', AEPut p]
-    ps <- mapM (flip substIn subst) (map PVar $ _pvars c)
-    return (T.EFun f ps p, (ty, p), eff, subst)
+-- -- Rule 20
+-- translate' :: S.Expr -> TE (T.Expr, DecoratedType, Effects, Substitution)
+-- translate' expr = do
+--     (expr', decTy, eff, subst) <- translate expr
+--     eff' <- observe decTy eff
+--     let ps = frv (eff S.\\ eff')
+--     return (foldr T.ELetReg expr' ps, decTy, eff', emptySubst)
 
--- Rule 20
-translate' :: S.Expr -> TE (T.Expr, DecoratedType, Effects, Substitution)
-translate' expr = do
-    (expr', decTy, eff, subst) <- translate expr
-    eff' <- observe decTy eff
-    let ps = frv (eff S.\\ eff')
-    return (foldr T.ELetReg expr' ps, decTy, eff', emptySubst)
-
--- Utils
+-- -- Utils
 
 isInstanceOf :: Type -> CompoundTS -> Maybe Substitution
 isInstanceOf = undefined
---
+++ src/Unify.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE TemplateHaskell, MultiParamTypeClasses,
+             TypeSynonymInstances, FlexibleInstances #-}
+
+module Unify where
+
+import Type
+import qualified Data.Map as M
+import qualified Data.Set as S
+import Control.Lens
+import Common
+
+data Substitution = Substitution {
+  _placeSubst  :: M.Map Name Place
+, _typeSubst   :: M.Map Name Type
+, _effectSubst :: M.Map Name (Name, Effects)
+}
+
+makeLenses ''Substitution
+
+class Unify a where
+    unify :: a -> a -> Maybe Substitution
+
+instance Unify Type where
+    unify TInt TInt  = Just emptySubst
+    unify (TVar x) t = Just $ emptySubst { _typeSubst = M.singleton x t }
+    unify t (TVar x) = Just $ emptySubst { _typeSubst = M.singleton x t }
+    unify (TArrow (ty1,  p1)  (evar,  effs)  (ty2,  p2))
+          (TArrow (ty1', p1') (evar', effs') (ty2', p2')) = do
+          s <- unify ty1 ty1'
+          s <- flip unionSubst s <$> unify (p1 `substIn` s) p1'
+          s <- flip unionSubst s <$> unify (ty2 `substIn` s) ty2'
+          s <- flip unionSubst s <$> unify (p2 `substIn` s) p2'
+          let effs'' = effs `S.union` effs'
+          return (effectSubst %~ (M.insert evar  (evar, effs'') .
+                                  M.insert evar' (evar, effs'')) $ s)
+    unify _ _        = Nothing
+
+
+instance Unify Place where
+    unify (PVar x) p = Just $ emptySubst { _placeSubst = M.singleton x p }
+    unify p (PVar x) = Just $ emptySubst { _placeSubst = M.singleton x p }
+    unify (PReg r1) (PReg r2) | r1 == r2 = Just emptySubst
+    unify _ _ = Nothing
+
+
+unionSubst :: Substitution -> Substitution -> Substitution
+unionSubst = undefined
+
+emptySubst :: Substitution
+emptySubst = undefined
+
+class Subst a b where
+    substIn :: a -> Substitution -> b
+
+instance Subst Type Type where
+    substIn = undefined
+
+instance Subst Place Place where
+    substIn = undefined
+
+instance Subst Name (Name, Effects) where
+    substIn = undefined
+
